b8ab88da2c421bb519bde8f9ce44aac8795c26fc
phisher13 <channektoshka@gmail.com>
2022-11-01 19:38:32 +0200

    [ADD]: authorization service

--- internal/domain/auth/service/service.go
+++ internal/domain/auth/service/service.go
@@ -1,1 +1,22 @@
 package service
+
+import (
+	"github.com/phisher13/go-api/internal/domain/auth/entity"
+	"github.com/phisher13/go-api/internal/domain/auth/storage"
+)
+
+type service struct {
+	store storage.Authorization
+}
+
+func NewAuthService(store storage.Authorization) AuthService {
+	return &service{store: store}
+}
+
+func (s *service) GetUser(username, passwordHash string) (entity.UserModel, error) {
+	return s.store.GetUser(username, passwordHash)
+}
+
+func (s *service) CreateUser(dto *entity.UserDTO) (string, error) {
+	return s.store.CreateUser(dto)
+}
